1b2bcbddb42e4773db3e2c4ea525e7978f73d868
Raja Rahul <maddula.rajarahul@gmail.com>
2020-06-01 12:06:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,83 @@
-module.exports = () => {
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+module.exports = (mf) => {
+	var express = require('express');
+	var app = express();
+	var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+	
+	app.use(bodyParser.urlencoded({ extended: false }));
+	
+	// parse application/json
+	
+	app.use(bodyParser.json());
+	
+	app.get('/beneficiary', function (req, res) {
 
-// parse application/json
+		console.log("MF ---> " + JSON.stringify(mf) );
 
-app.use(bodyParser.json());
-
-app.get('/beneficiary', function (req, res) {
-	console.log(">>>---------------- 1. headers ------------");
-	console.log(JSON.stringify(req.headers));
-	console.log("");
-
-	console.log("---------------- 2. params ------------");
-	console.log(req.query);
-	console.log("");
-
-
-	if (req.body) {
-		console.log("---------------- 3. Body ------------");
-		console.log(JSON.stringify(req.body));
-	}
-
-	console.log("---------------- 4. get");
-	res.json([{ "firstName": "Ajay", "lastName": "Kapoor", "bankName": "ABCD", "accountNumber": "1486423458200" }, { "firstName": "Rakesh", "lastName": "Sharma", "bankName": "PQRS", "accountNumber": "0386423456235" }]);
-
-	console.log("<<---------------------------");
-})
-
-app.post('/beneficiary', function (req, res) {
-
-	// console.log(">>>---------------- 1. headers ------------");
-	// console.log(JSON.stringify(req.headers));	
-	// console.log("");
-
-	// console.log("---------------- 2. params ------------");
-	// console.log(req.query);	
-	// console.log("");
-
-
-	// if (req.body){
-	// 	console.log("---------------- 3. Body ------------");	
-	// 	console.log(JSON.stringify(req.body));	
-	// }
-
-	//console.log(req.body);
-
-
-	console.log(req.body);
-
-	console.log(JSON.stringify(req.headers));
-	console.log(">>> post");
-	res.json({});
-});
-
-
-app.put('/beneficiary', function (req, res) {
-	console.log(JSON.stringify(req.headers));
-	console.log(">>> put");
-	res.json({});
-});
-
-
-app.delete('/beneficiary', function (req, res) {
-	console.log(JSON.stringify(req.headers));
-	console.log(">>> delete");
-	res.json({});
-});
-
-var port = process.env.PORT || 7000;
-app.listen(port);
-console.log('Node.js web server at port 7000 is running..');
-
-console.log("checking changes ----> server in exports");
-return app;
-};
+		console.log(">>>---------------- 1. headers ------------");
+		console.log(JSON.stringify(req.headers));
+		console.log("");
+	
+		console.log("---------------- 2. params ------------");
+		console.log(req.query);
+		console.log("");
+	
+	
+		if (req.body) {
+			console.log("---------------- 3. Body ------------");
+			console.log(JSON.stringify(req.body));
+		}
+	
+		console.log("---------------- 4. get");
+		res.json([{ "firstName": "Ajay", "lastName": "Kapoor", "bankName": "ABCD", "accountNumber": "1486423458200" }, { "firstName": "Rakesh", "lastName": "Sharma", "bankName": "PQRS", "accountNumber": "0386423456235" }]);
+	
+		console.log("<<---------------------------");
+	})
+	
+	app.post('/beneficiary', function (req, res) {
+	
+		// console.log(">>>---------------- 1. headers ------------");
+		// console.log(JSON.stringify(req.headers));	
+		// console.log("");
+	
+		// console.log("---------------- 2. params ------------");
+		// console.log(req.query);	
+		// console.log("");
+	
+	
+		// if (req.body){
+		// 	console.log("---------------- 3. Body ------------");	
+		// 	console.log(JSON.stringify(req.body));	
+		// }
+	
+		//console.log(req.body);
+	
+	
+		console.log(req.body);
+	
+		console.log(JSON.stringify(req.headers));
+		console.log(">>> post");
+		res.json({});
+	});
+	
+	
+	app.put('/beneficiary', function (req, res) {
+		console.log(JSON.stringify(req.headers));
+		console.log(">>> put");
+		res.json({});
+	});
+	
+	
+	app.delete('/beneficiary', function (req, res) {
+		console.log(JSON.stringify(req.headers));
+		console.log(">>> delete");
+		res.json({});
+	});
+	
+	var port = process.env.PORT || 7000;
+	app.listen(port);
+	console.log('Node.js web server at port 7000 is running..');
+	
+	console.log("checking changes ----> server in exports");
+	return app;
+	};
