2233b0b68131af035218a4424d80e0d7c33c358f
Raja Rahul <maddula.rajarahul@gmail.com>
2020-06-01 16:31:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ module.exports = (mf) => {
 	var app = express();
 	var bodyParser = require('body-parser');
 // 	var mf = require('../mf.js')();
-	var mf = mf();
 	console.log("This is MF as a param ---> " + JSON.stringify(mf));
 
 	
